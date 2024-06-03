fbe0dcf4b726ebc709b7b85a76cceb786d424e28
XiaoMing <18300180089@fudan.edu.cn>
2024-06-03 16:44:22 +0800

    在创建表单之前的工作，包括路由选择，使用中间件

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module My_goblog
 
 go 1.22.3
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -3,23 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+	"html/template"
+    "unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
+type ArticlesFormData struct{
+    Title, Body string
+    URL *url.URL
+    Errors map[string]string
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
     fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
     fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
         "<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
     w.WriteHeader(http.StatusNotFound)
     fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
@@ -35,28 +44,108 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "创建新的文章")
+    title := r.PostFormValue("title")
+    body := r.PostFormValue("body")
+
+    errors := make(map[string]string)
+
+    if title == ""{
+        errors["title"] = "标题不能为空"
+    } else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+        errors["title"] = "标题长度需介于 3-40"
+    }
+
+    if body == "" {
+        errors["body"] = "内容不能为空"
+    } else if utf8.RuneCountInString(body) < 10 {
+        errors["body"] = "内容长度需大于或等于 10 个字节"
+    }
+
+    if len(errors) == 0 {
+        fmt.Fprint(w, "验证通过!<br>")
+    } else {
+        storeURL, _ := router.Get("article.store").URL()
+
+        data := ArticlesFormData{
+            Title: title,
+            Body: body,
+            URL: storeURL,
+            Errors: errors,
+        }
+        tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+        if err != nil {
+            panic(err)
+        }
+
+        err = tmpl.Execute(w, data)
+        if err != nil {
+            panic(err)
+        }
+    }
 }
 
 
-func main() {
-    router := mux.NewRouter()
 
+
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request){
+    storeURL, _ := router.Get("articles.store").URL()
+    data := ArticlesFormData{
+        Title:  "",
+        Body:   "",
+        URL:    storeURL,
+        Errors: nil,
+    }
+
+    tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+    if err != nil {
+        panic(err)
+    }
+
+    err = tmpl.Execute(w,data)
+
+    if err !=nil {
+        panic(err)
+    }
+
+}
+
+
+
+func forceHTMLMiddleware(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+        h.ServeHTTP(w, r)
+    })
+}
+
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+        if r.URL.Path != "/" {
+            r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
+
+
+
+func main() {
+    
     router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
     router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
     router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
     router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
     router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+    router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
     // 自定义 404 页面
     router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
-    // 通过命名路由获取 URL 示例
-    homeURL, _ := router.Get("home").URL()
-    fmt.Println("homeURL: ", homeURL)
-    articleURL, _ := router.Get("articles.show").URL("id", "23")
-    fmt.Println("articleURL: ", articleURL)
+    router.Use(forceHTMLMiddleware)
 
-    http.ListenAndServe(":3000", router)
+    http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
